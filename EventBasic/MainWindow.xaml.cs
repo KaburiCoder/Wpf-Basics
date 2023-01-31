62b92b17bf69b083744159a7e26b7f0e171b3fdb
KaburiCorder <specialemail90@gmail.com>
2023-01-31 23:10:41 +0900

    버블링, 터널링 강의 소스코드

--- EventBasic/MainWindow.xaml.cs
+++ EventBasic/MainWindow.xaml.cs
@@ -20,9 +20,66 @@ namespace EventBasic
   /// </summary>
   public partial class MainWindow : Window
   {
+    private List<string> list = new List<string>();
     public MainWindow()
     {
       InitializeComponent();
+    }
+
+    private void SetBubblingTextBox()
+    {
+      txtBubbling.Text= string.Join(" > ", list);
+      ClearList();
+    }
+
+    private void SetTunnelingTextBox()
+    {
+      txtTunneling.Text = string.Join(" > ", list);
+      ClearList();
+
+    }
+    private async void ClearList()
+    {
+      await Task.Delay(100);
+      list.Clear(); 
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      list.Add("Button-Gray");
+      SetBubblingTextBox();
+    }
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      list.Add("Border-Blue");
+      SetBubblingTextBox();
+      e.Handled = true;
+    }
+
+    private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      list.Add("Rectangle-Red");
+      SetBubblingTextBox();
+    }
+
+    private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+      list.Add("Button-Gray");
+      SetTunnelingTextBox();
+      e.Handled = true;
+    }
+
+    private void Border_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+      list.Add("Border-Blue");
+      SetTunnelingTextBox();
+    }
+
+    private void Rectangle_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+      list.Add("Rectangle-Red");
+      SetTunnelingTextBox();
     }
   }
 }
--- EventBasic/obj/Debug/net6.0-windows/MainWindow.g.cs
+++ EventBasic/obj/Debug/net6.0-windows/MainWindow.g.cs
@@ -69,7 +69,7 @@ namespace EventBasic {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EventBasic;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EventBasic;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
