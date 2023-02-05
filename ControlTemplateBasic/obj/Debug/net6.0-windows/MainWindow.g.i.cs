bf6d477a6e455714748b4c6b9df3084ce0783706
KaburiCorder <specialemail90@gmail.com>
2023-02-05 23:20:16 +0900

    Property Trigger 강의 자료

--- ControlTemplateBasic/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ ControlTemplateBasic/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace ControlTemplateBasic {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ControlTemplateBasic;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ControlTemplateBasic;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
