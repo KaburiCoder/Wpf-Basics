a0c2ab26ab15f22e9c7fa2a8f2b84e1883b1a0c5
KaburiCorder <specialemail90@gmail.com>
2023-02-10 00:01:26 +0900

    Data Trigger 강의 자료

--- EventTriggerBasic/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ EventTriggerBasic/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace EventTriggerBasic {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EventTriggerBasic;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EventTriggerBasic;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
