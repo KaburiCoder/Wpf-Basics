9802c9a1d585db9776ec2ad67dbbc7d0bbbcbfc1
KaburiCorder <specialemail90@gmail.com>
2023-03-22 00:08:23 +0900

    WPF RichTextBox 강의자료

--- PropertyTriggerBaisc/obj/Debug/net6.0-windows/App.g.i.cs
+++ PropertyTriggerBaisc/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace ControlTemplateBasic {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ControlTemplateBasic {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public static void Main() {
             ControlTemplateBasic.App app = new ControlTemplateBasic.App();
             app.InitializeComponent();
