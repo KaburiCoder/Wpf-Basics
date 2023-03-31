c5bfa4b96d90bbaec283d6dd7bf700eeaddbfdec
KaburiCorder <specialemail90@gmail.com>
2023-03-31 23:50:15 +0900

    Attached Property 강의자료 업로드

--- WpfOxyPlotGraph/obj/Debug/net7.0-windows/Views/MainView.g.i.cs
+++ WpfOxyPlotGraph/obj/Debug/net7.0-windows/Views/MainView.g.i.cs
@@ -62,7 +62,7 @@ namespace WpfOxyPlotGraph.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfOxyPlotGraph;component/views/mainview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfOxyPlotGraph;V1.0.0.0;component/views/mainview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
